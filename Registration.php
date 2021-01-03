ed4116b979f1061a78cd8792f735f348464ca771
nishthabhakta <68797584+nishthabhakta@users.noreply.github.com>
2021-01-02 18:32:06 -0600

    Add files via upload

---
+++ Registration.php
@@ -0,0 +1,144 @@
+
+
+
+
+<html>
+		<body>
+		<form method="post" enctype="multipart/form-data">
+			<table align="center" border="2px">
+			<th>Enter your information here</th>
+					<tr>
+					<td>First Name</td>
+					<td><input type="text" id="fname"/></td>
+					
+					
+				</tr>
+				
+				<tr>
+				<td>
+				Last Name
+				</td>
+				     <td>
+					 <input type="text" id="lname"/>
+					 </td>
+				</tr>
+				
+					<tr>
+				<td>
+				Email Address
+				</td>
+				     <td>
+					 <input type="text" id="email"/>
+					 </td>
+				</tr>
+					
+					<tr>
+				<td>
+				Password
+				</td>
+				     <td>
+					 <input type="text" id="pass"/>
+					 </td>
+				</tr>
+					
+					<tr>
+				<td>
+				Confirm Password
+				</td>
+				     <td>
+					 <input type="text" id="cpass"/>
+					 </td>
+				</tr>
+					<tr>
+				<td>
+				Address
+				</td>
+				     <td>
+					 <input type="text" id="address"/>
+					 </td>
+				</tr>
+				
+				
+				<tr>
+				<td>
+				City
+				</td>
+				     <td>
+					 <input type="text" id="city"/>
+					 </td>
+				</tr>
+				
+				<tr>
+				<td>
+				State
+				</td>
+				     <td>
+					 <input type="text" id="state"/>
+					 </td>
+				</tr>
+				
+				<tr>
+				<td>
+				Country
+				</td>
+				     <td>
+					 <select>
+					 <option>United States</option>
+					 <option>Canada</option>
+					 </select>
+					 </td>
+				</tr>
+				
+				<tr>
+				<td>
+				Date of Birth
+				</td>
+				     <td>
+					 <input type="Date" id="dob"/>
+					 </td>
+				</tr>
+				
+				<tr>
+				<td>
+				Age
+				</td>
+				     <td>
+					 <input type="text" id="age"/>
+					 </td>
+				</tr>
+				
+				
+				
+						<tr>
+				<td>
+				Gender
+				</td>
+				     <td>
+					  <input type="radio" id="male" name="gender" value="male">
+<label for="male">Male</label>
+<input type="radio" id="female" name="gender" value="female">
+<label for="female">Female</label>
+<input type="radio" id="other" name="gender" value="other">
+<label for="other">Other</label> 
+					 </td>
+				</tr>
+				
+				
+				
+						<tr>
+				<td>
+				<input type="submit" id="submit"/>
+				</td>
+				     
+				</tr>
+				 
+			</table>
+			<form>
+			
+		</body>
+		
+</html>
+<?php
+
+/* code for insert query in database */
+?>
